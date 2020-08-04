0444bc24f9c3021d695e477a831c42e514cc2eaf
nibble0101 <mj.mawa@yahoo.com>
2020-08-04 11:02:39 +0300

    Added social media icons to the footer component

---
+++ src/footer/SocialMedia.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function SocialMedia() {
+  return (
+    <div className="social-media-wrapper">
+      <div>
+        <i class="fab fa-twitter"></i>
+      </div>
+      <div>
+        <i class="fab fa-github"></i>
+      </div>
+      <div>
+        <i class="fab fa-github"></i>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
