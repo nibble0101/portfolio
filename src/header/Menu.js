ec10f3d7fb38a86e2325f4c1c418b26f759c0516
nibble0101 <mj.mawa@yahoo.com>
2020-08-20 19:57:13 +0300

    Finished working on Hamburger menu

--- src/header/Menu.js
+++ src/header/Menu.js
@@ -1,29 +1,60 @@
-import React from "react";
+import React, { useState, useCallback, useRef } from "react";
+import HamburgerMenu from "react-hamburger-menu";
 function Menu(props) {
+  const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef();
+  const hamburgerClickHandle = useCallback((e) => {
+    setIsOpen((prevState) => !prevState);
+    menuRef.current.classList.toggle('display-menu');
+    document.body.classList.toggle('lock-scroll');
+  }, []) 
+  const menuClickHandle = useCallback(e => {
+       const {name} = e.target;
+       if(name === "link"){
+        menuRef.current.classList.toggle('display-menu');
+        document.body.classList.toggle('lock-scroll');
+        setIsOpen((prevState) => !prevState);
+       }
+  }, [])
   return (
-    <ul className="menu">
-      <li>
-        <a href="#home"> HOME</a>
-      </li>
-      <li>
-        <a href="#about"> ABOUT </a>
-      </li>
-      <li>
-        <a href="#projects"> PROJECTS</a>
-      </li>
-      <li>
-        <a href="#contact"> CONTACT </a>
-      </li>
-      <li>
-        <a
-          href="https://dev.to/nibble0101"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          BLOG
-        </a>
-      </li>
-    </ul>
+    <React.Fragment>
+      <ul className="menu" ref = {menuRef} onClick = {menuClickHandle} >
+        <li>
+          <a href="#home" name = "link"> HOME</a>
+        </li>
+        <li>
+          <a href="#about" name = "link"> ABOUT </a>
+        </li>
+        <li>
+          <a href="#projects" name = "link"> PROJECTS</a>
+        </li>
+        <li>
+          <a href="#contact" name = "link"> CONTACT </a>
+        </li>
+        <li>
+          <a
+            href="https://dev.to/nibble0101"
+            target="_blank"
+            rel="noreferrer noopener"
+            name = "link"
+          >
+            BLOG
+          </a>
+        </li>
+      </ul>
+      <HamburgerMenu
+        isOpen={isOpen}
+        menuClicked={hamburgerClickHandle}
+        width={18}
+        height={15}
+        strokeWidth={1}
+        rotate={0}
+        color="gray"
+        borderRadius={0}
+        animationDuration={0.5}
+        className="hamburger-menu"
+      />
+    </React.Fragment>
   );
 }
 
