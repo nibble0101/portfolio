2c15ff4c19a5587971d600ef6f124ffb044c50e4
nibble0101 <mj.mawa@yahoo.com>
2020-08-20 15:59:43 +0300

    Added more styling to form

--- src/main/Main.js
+++ src/main/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import Home from "./home/Home";
 import About from "./about/About";
 import Projects from "./projects/Projects";
+import Contact from "./contact/Contact";
 
 function Main(props) {
   return (
@@ -9,6 +10,7 @@ function Main(props) {
       <Home />
       <About />
       <Projects />
+      <Contact />
     </React.Fragment>
   );
 }
--- src/main/contact/Contact.js
+++ src/main/contact/Contact.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 function Contact(props) {
-    return <section>
+    return <section className = "contact-form-wrapper">
+        <h2 id = "contact"> GET IN TOUCH </h2>
         <form action = "POST" onSubmit = {() => null} >
-             
+            <input type = "text" name = "name" placeholder = "Name"  required />
+            <input type = "email" name = "email" placeholder = "Email"  required />
+            <textarea  name = "message" placeholder = "Message"  /> 
+            <input type = "submit" value = "SEND" />
         </form>
     </section>
     
