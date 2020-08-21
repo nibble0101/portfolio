092f1be5ae6bc087693a86aa612130fa0c51475a
nibble0101 <mj.mawa@yahoo.com>
2020-08-21 17:32:43 +0300

    Added more styling

--- src/main/home/Home.js
+++ src/main/home/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
+import ScrollAnimation from "react-animate-on-scroll";
 
 function Home(props) {
   return (
-    <section  className="home" id = "home">
+    <section className="home" id="home">
       <div className="about-section">
         <h1>
           HI. MY NAME IS <span className="name">JOSEPH MAWA</span>
