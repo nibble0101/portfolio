c46729afd96a6a927ab1d61940567a5bc2a35e33
nibble0101 <mj.mawa@yahoo.com>
2020-08-21 23:05:51 +0300

    Added more styling to arrow button

--- src/main/home/Home.js
+++ src/main/home/Home.js
@@ -2,6 +2,9 @@ import React from "react";
 import Socials from "../../footer/Socials";
 
 function Home(props) {
+  const arrowClickHandle = () => {
+      document.getElementById('about').scrollIntoView();
+  }
   return (
     <section className="home" id="home">
       <div className="about-section">
@@ -11,7 +14,7 @@ function Home(props) {
         <p>I AM A FULL STACK DEVELOPER!</p>
       </div>
       <div className="arrow-wrapper">
-        <button className="arrow-button">
+        <button className="arrow-button" onClick = {arrowClickHandle}>
           <span className="inner-wrapper">&#8595;</span>
         </button>
       </div>
