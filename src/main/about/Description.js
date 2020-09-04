85b49e07a3fd04608a8a439b06ec5aa261f442ea
nibble0101 <mj.mawa@yahoo.com>
2020-09-04 15:55:16 +0300

    Edited about me section

--- src/main/about/Description.js
+++ src/main/about/Description.js
@@ -5,24 +5,16 @@ function Description() {
   return (
     <section className="description">
       <div id="about"></div>
-      <h2 >ABOUT ME</h2>
+      <h2>ABOUT ME</h2>
       <p>
-        My name is Joseph Mawa, a web developer from Uganda. I started coding as
-        a hobby after graduating college approximately {year - 2012} years ago.
-        The first language i was introduced to was python. I also toyed with R
-        and C before discovering Javascript. Since then i fell in love with web
-        development. At school, i studied Physics and Education. I worked as a
-        high school teacher before quitting to learn web development full time.
-        I am always working towards making myself a better developer. In my
-        spare time, you will either find me watching a movie or listening to {" "}
-        <a
-          href="https://www.recklessradio.ug/"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          music
-        </a>
-        .
+        My name is Joseph Mawa, a web developer. I started coding as a hobby
+        after graduating college.The first language i was introduced to was
+        python. I also toyed with R and C before discovering Javascript. Since
+        then i fell in love with web development. At school, i studied Physics
+        and Education. I worked as a high school teacher before quitting to
+        learn web development full time. I am always working towards making
+        myself a better developer. In my spare time, you will either find me
+        watching a movie or listening to music.
       </p>
       <h2>SKILLS</h2>
     </section>
