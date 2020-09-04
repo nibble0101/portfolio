12419078b7f6e574ad06e9dc778576143812f4d9
nibble0101 <mj.mawa@yahoo.com>
2020-09-04 15:26:50 +0300

    Added more styling to the portfolio

--- src/main/about/Description.js
+++ src/main/about/Description.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function Description() {
-  const year = new Date().getFullYear();
   return (
     <section className="description">
       <div id="about"></div>
@@ -9,13 +8,13 @@ function Description() {
       <p>
         My name is Joseph Mawa, a front-end developer slowly transitioning to
         fullstack developer. I started coding as a hobby after graduating
-        college approximately {year - 2012} years ago. The first language i was
-        introduced to was python. I also toyed briefly with R and C before discovering
-        Javascript. Since then i fell in love with web development. At school, i
-        studied Physics and Education. I worked as a high school teacher before
-        quitting to learn web development full time. I am always working towards
-        making myself a better developer. In my spare time, you will either find
-        me watching a movie or listening to music.
+        college. The first language i was introduced to was python. I also toyed
+        briefly with R and C before discovering Javascript. Since then i fell in
+        love with web development. At school, i studied Physics and Education. I
+        worked as a high school teacher before quitting to learn web development
+        full time. I am always working towards making myself a better developer.
+        In my spare time, you will either find me watching a movie or listening
+        to music.
       </p>
       <h2>SKILLS</h2>
     </section>
