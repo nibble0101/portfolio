34f1c69cbb1c5a04f6641478f56302c3c8774b9e
nibble0101 <mj.mawa@yahoo.com>
2021-04-15 19:45:24 +0300

    Add netlify form submission logic using state

--- src/main/contact/Contact.js
+++ src/main/contact/Contact.js
@@ -1,16 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
+
+const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
 
 function Contact(props) {
-    return <section className = "contact-form-wrapper">
-        <h2> GET IN TOUCH </h2>
-        <form name = "contact" action = "POST" data-netlify = "true">
-            <input type = "text" name = "name" placeholder = "Name" autoComplete  required />
-            <input type = "email" name = "email" placeholder = "Email" autoComplete required />
-            <textarea  name = "message" placeholder = "Message" required /> 
-            <input type = "submit" value = "SEND" />
-        </form>
+  const [formFields, setFormFields] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const onChangeHandler = (event) => {
+      const { name, value } = event.target;
+      setFormFields(prevValues => ({...prevValues, [name]: value }));
+  }
+  const onSubmitFormHandler = (event) => {
+      fetch("/", {
+        method: "POST",
+        headers: {"Content-Type": "application/x-www-form-urlencoded"},
+        body: encode({ "form-name": "contact", ...formFields })
+      }).then(() => alert("Success"))
+      .catch(err => console.log(err))
+      event.preventDefault();
+  }
+  return (
+    <section className="contact-form-wrapper">
+      <h2> GET IN TOUCH </h2>
+      <form onSubmit = {onSubmitFormHandler}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={formFields.name}
+          onChange = {onChangeHandler}
+          autoComplete="true"
+          required
+        />
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          value={formFields.email}
+          onChange = {onChangeHandler}
+          autoComplete="true"
+          required
+        />
+        <textarea
+          name="message"
+          placeholder="Message"
+          value={formFields.message}
+          onChange = {onChangeHandler}
+          autoComplete="true"
+          required
+          
+        />
+        <input type="submit" value="SEND" />
+      </form>
     </section>
-    
+  );
 }
 
 export default Contact;
