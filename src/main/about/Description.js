3ca0ee8ff2803211be181d4d3642690f7d598423
nibble0101 <mj.mawa@yahoo.com>
2021-04-30 11:04:57 +0300

    Reorganized project description

--- src/main/about/Description.js
+++ src/main/about/Description.js
@@ -11,10 +11,21 @@ function Description() {
         introduced to was python. I also toyed with R and C before discovering
         Javascript. Since then I fell in love with web development. At school, I
         studied Physics and Education. I worked as a high school teacher before
-        quitting to learn web development full time. I am always working towards
-        making myself a better developer and technical writer. In my spare time,
-        you will either find me watching a movie or listening to{" "}
-        <a href="http://radio.garden">music</a>.
+        quitting to learn web development full time. In my spare time, you will
+        most likely find me watching a movie, helping out folks at{" "}
+        <a href="https://forum.freecodecamp.org/u/nibble/activity">
+          freeCodeCamp
+        </a>{" "}
+        or listening to <a href="http://radio.garden">music</a>. I also love
+        contributing to{" "}
+        <a href="https://github.com/ocaml/ocaml.org">Ocaml.org</a>,
+        <a href="https://disease.sh"> disease.sh</a>{" "}
+        and <a href="https://github.com/wechaty/wechaty">Wechaty</a>. You are
+        welcome to take a look at my{" "}
+        <a href="https://docs.google.com/document/d/1W6m5UR9PCvMOI8Pmjiw7k0rl-bh3PVJK_zbWNXVE6d0/edit?usp=sharing">
+          resume
+        </a>
+        .
       </p>
       <h2>SKILLS</h2>
     </section>
--- src/main/projects/projects-description.js
+++ src/main/projects/projects-description.js
@@ -1,48 +1,58 @@
 const projects = [
   {
     name: "COVIWEB",
-    imageUrl: "https://github.com/nibble0101/portfolio/blob/master/src/main/projects/images/coviweb.jpg?raw=true",
+    imageUrl:
+      "https://github.com/nibble0101/portfolio/blob/master/src/main/projects/images/coviweb.jpg?raw=true",
     altText: "coviWeb",
     projectUrl: "https://coviweb.netlify.app/",
     sourceCodeUrl: "https://github.com/nibble0101/coviweb",
     key: "coviweb",
-    description: "coviWeb is a Single Page Application (SPA) which displays up-to-date COVID-19 statistics. It was built using React.js, d3.js and CSS.",
-  },
-  {
-    name: "FILMMAZE",
-    imageUrl: "https://github.com/nibble0101/portfolio/blob/master/src/main/projects/images/filmmaze.jpg?raw=true",
-    altText: "filMaze",
-    projectUrl: "https://filmmaze.netlify.app/",
-    sourceCodeUrl: "https://github.com/nibble0101/FilmMaze",
-    key: "filmMaze",
-    description: "FilmMaze is a Single Page Application(SPA) which displays a list of Movies, TV Shows and Personalities in the movie industry. It was built using React.js and CSS.",
-  },
-  {
-    name: "CLOUDYGEM",
-    imageUrl: "https://github.com/nibble0101/portfolio/blob/master/src/main/projects/images/cloudygem.png?raw=true",
-    altText: "cloudyGem",
-    projectUrl: "https://cloudygem.netlify.app/",
-    sourceCodeUrl: "https://github.com/nibble0101/cloudyGem",
-    key: "cloudyGem",
-    description: "This is a weather app which displays weather information for a given city/town for the next five days. It is a single page application(SPA) built with React.js and CSS.",
+    description:
+      "coviWeb is a Single Page Application (SPA) which displays up-to-date COVID-19 statistics. It was built using React.js, d3.js and CSS.",
   },
   {
     name: "WHIZZNIAC",
-    imageUrl: "https://github.com/nibble0101/portfolio/blob/master/src/main/projects/images/whizzniac.jpg?raw=true",
+    imageUrl:
+      "https://github.com/nibble0101/portfolio/blob/master/src/main/projects/images/whizzniac.jpg?raw=true",
     altText: "whizzNiac",
     projectUrl: "https://whizzniac.netlify.app/",
     sourceCodeUrl: "https://github.com/nibble0101/whizzniac",
     key: "whizzNiac",
-    description: "whizzNiac is a Quiz App written in react.js. It consists of quiz from different subject areas with four or two possible solutions whith only one solution correct.",
+    description:
+      "whizzNiac is a Quiz App written in react.js. It consists of quiz from different subject areas with four or two possible solutions whith only one solution correct.",
+  },
+  {
+    name: "FILMMAZE",
+    imageUrl:
+      "https://github.com/nibble0101/portfolio/blob/master/src/main/projects/images/filmmaze.jpg?raw=true",
+    altText: "filMaze",
+    projectUrl: "https://filmmaze.netlify.app/",
+    sourceCodeUrl: "https://github.com/nibble0101/FilmMaze",
+    key: "filmMaze",
+    description:
+      "FilmMaze is a Single Page Application(SPA) which displays a list of Movies, TV Shows and Personalities in the movie industry. It was built using React.js and CSS.",
+  },
+  {
+    name: "CLOUDYGEM",
+    imageUrl:
+      "https://github.com/nibble0101/portfolio/blob/master/src/main/projects/images/cloudygem.png?raw=true",
+    altText: "cloudyGem",
+    projectUrl: "https://cloudygem.netlify.app/",
+    sourceCodeUrl: "https://github.com/nibble0101/cloudyGem",
+    key: "cloudyGem",
+    description:
+      "This is a weather app which displays weather information for a given city/town for the next five days. It is a single page application(SPA) built with React.js and CSS.",
   },
   {
     name: "HTML CODES",
-    imageUrl: "https://github.com/nibble0101/portfolio/blob/master/src/main/projects/images/htmlcodegenerator.png?raw=true",
+    imageUrl:
+      "https://github.com/nibble0101/portfolio/blob/master/src/main/projects/images/htmlcodegenerator.png?raw=true",
     altText: "htmlcodegenerator",
     projectUrl: "https://htmlcodegenerator.netlify.app/",
     sourceCodeUrl: "https://github.com/nibble0101/HTML-Codes",
     key: "htmlcodegenerator",
-    description: "htmlCodes is a simple app which displays a list of characters and their corresponding HTML codes. It was built using html, vanilla javascript and CSS",
+    description:
+      "htmlCodes is a simple app which displays a list of characters and their corresponding HTML codes. It was built using html, vanilla javascript and CSS",
   },
 ];
 
